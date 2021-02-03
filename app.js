b367f1497e0aa98c97c595b37a58fbccd915a432
Daniele Esposito <esp.dne@gmail.com>
2021-02-03 17:09:26 +0100

    testheroku

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get('/',(req,res)=>{
+res.send('hello world');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server run at port ${PORT}`));
